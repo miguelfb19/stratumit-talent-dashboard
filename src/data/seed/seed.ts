fb6a3599f5eb3fa4b501e1fd393f656a9db4f002
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-02-05 19:14:26 -0500

    modify to-do

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,42 +1,42 @@
-// import prisma from "@/lib/prisma";
-// import { languajes, technologies, users } from "@/data/seed/seed-data";
+import prisma from "../../lib/prisma";
+import { languajes, technologies, users } from "./seed-data";
 
 async function main() {
-  //     // First delete all the data
-  //   await prisma.profileTechnologies.deleteMany()
-  //   await prisma.profileLanguajes.deleteMany()
-  //   await prisma.languaje.deleteMany();
-  //   await prisma.technology.deleteMany();
-  //   await prisma.jobExperiences.deleteMany()
-  //   await prisma.educationalProject.deleteMany()
-  //   await prisma.profile.deleteMany();
-  //   await prisma.user.deleteMany();
+      // First delete all the data
+    await prisma.profileTechnologies.deleteMany()
+    await prisma.profileLanguajes.deleteMany()
+    await prisma.languaje.deleteMany();
+    await prisma.technology.deleteMany();
+    await prisma.jobExperiences.deleteMany()
+    await prisma.educationalProject.deleteMany()
+    await prisma.profile.deleteMany();
+    await prisma.user.deleteMany();
 
-  //   // Create seed data
-  //   await prisma.user.createMany({
-  //     data: users,
-  //   });
+    // Create seed data
+    await prisma.user.createMany({
+      data: users,
+    });
 
-  //   // Create languajes
-  //   for (const languaje of languajes) {
-  //     await prisma.languaje.create({
-  //       data: {
-  //         name: languaje,
-  //       },
-  //     });
-  //   }
+    // Create languajes
+    for (const languaje of languajes) {
+      await prisma.languaje.create({
+        data: {
+          name: languaje,
+        },
+      });
+    }
 
-  //   // Create technologies with his respective category
-  //   technologies.map(async (technology) => {
-  //     const { category, name } = technology;
+    // Create technologies with his respective category
+    technologies.map(async (technology) => {
+      const { category, name } = technology;
 
-  //     await prisma.technology.createMany({
-  //       data: {
-  //         name,
-  //         category,
-  //       },
-  //     });
-  //   });
+      await prisma.technology.createMany({
+        data: {
+          name,
+          category,
+        },
+      });
+    });
 
   console.log("seed executed");
 }
