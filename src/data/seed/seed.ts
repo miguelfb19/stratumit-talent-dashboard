239501b6944bd136957ae2495e07a687059ef52f
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-02-11 20:07:15 -0500

    create supabase and connect

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -26,7 +26,7 @@ async function main() {
   }
 
   // Create technologies with his respective category
-  technologies.map(async (technology) => {
+  for (const technology of technologies) {
     const { category, name } = technology;
 
     await prisma.technology.createMany({
@@ -35,7 +35,7 @@ async function main() {
         category,
       },
     });
-  });
+  }
 
   // Create and get permissions and roles
   await prisma.permission.createMany({
@@ -52,21 +52,16 @@ async function main() {
   // Assignement role permissions
 
   await prisma.rolePermission.createMany({
-    data: rolePermissions.flatMap(
-      (rolPerm) => {
-        return rolPerm.permissions.map((perm) => {
-          return {
-            roleId: rolesdDB.find((role) => role.name === rolPerm.role)!.id,
-            permissionId: permissionsDB.find(
-              (permission) => permission.name === perm,
-            )!.id,
-          };
-        });
-      },
-
-      // roleId: rolesdDB.find((role) => role.name === rolPerm.role)!.id,
-      // permissionId:
-    ),
+    data: rolePermissions.flatMap((rolPerm) => {
+      return rolPerm.permissions.map((perm) => {
+        return {
+          roleId: rolesdDB.find((role) => role.name === rolPerm.role)!.id,
+          permissionId: permissionsDB.find(
+            (permission) => permission.name === perm
+          )!.id,
+        };
+      });
+    }),
   });
 
   // Create users seed data
@@ -99,7 +94,7 @@ async function main() {
           },
         },
       });
-    }),
+    })
   );
 
   //eslint-disable-next-line
